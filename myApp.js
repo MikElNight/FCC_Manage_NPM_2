ca4023fd594efe845ddaf699b60f4c053e874dfb
Luis Fonseca <luis.fonseca.work@gmail.com>
2022-02-04 23:16:57 +0000

    upd

--- myApp.js
+++ myApp.js
@@ -2,9 +2,11 @@
 var express = require('express');
 var app = express();
 
+var absolutePath = __dirname + "/views/index.html";
 
 app.get("/", (req, res) => {
     res.send("Hello Express");
+    res.sendFile(absolutePath);
   });
 
 

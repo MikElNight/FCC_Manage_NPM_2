619c2baa53502153ffeb03a2c9cabed45fa5c1b0
Luis Fonseca <luis.fonseca.work@gmail.com>
2022-02-04 23:45:40 +0000

    minor upd

--- myApp.js
+++ myApp.js
@@ -2,21 +2,16 @@
 var express = require('express');
 var app = express();
 
-//var absolutePath = __dirname + "/views/index.html";
+var absolutePath = __dirname + "/views/index.html";
 
 
 // Serve an HTML file
 app.get("/", (req, res) => {
     //res.send("Hello Express");
-    res.sendFile(__dirname + "/views/index.html");
+    res.sendFile(absolutePath);
   });
+
 
 // Serve Static Assets (images, scripts, CSS, etc) 
 app.use("/public", express.static(__dirname + "/public"));
 
-/*
-// Serve JSON file on a Specific Route
-app.get("/json", (req, res) => {
-    res.json({"message": "Hello json"});
-  });
-*/

d1194eb4bae784261bc30a4ca742185214d3a7e9
Luis Fonseca <luis.fonseca.work@gmail.com>
2022-02-06 00:49:30 +0000

    minor upd

--- myApp.js
+++ myApp.js
@@ -1,16 +1,54 @@
-//console.log("Hello World");
+require('dotenv').config();
+/**********************************************/
 var express = require('express');
 var app = express();
-
-// Serve an HTML File
+/**********************************************/
+// Implement a Root-Level Request Logger Middleware
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
+/**********************************************/
+// GET route handler for home URL ("/") that serves an HTML File ("/views/index.html")
 app.get("/", (req, res) => {
-    //res.send("Hello Express");
     res.sendFile(__dirname + "/views/index.html");
   });
-
-// Serve Static Assets  (stylesheets, scripts, images, etc.)
+/**********************************************/
+// USE Static Assets from directory (stylesheets, scripts, images, etc.)
 app.use("/public", express.static(__dirname + "/public"));
-
-
-
+/**********************************************/
+// GET route handler for ("/json") that serves a JSON file
+app.get("/json", (req, res) => {
+    // Check environment variables defined on the ".env" File
+    if (process.env.MESSAGE_STYLE=="uppercase") 
+        res.json({"message": "HELLO JSON"});
+    else res.json({"message": "Hello json"});
+  });
+/**********************************************/
+// GET route handler for ("/now") that Chain Middleware to Create a Time Server
+app.get("/now",
+    (req, res, next) => {
+      // adding a new property to req object in the middleware function
+      req.time = new Date().toString();
+      next();
+    },
+    (req, res) => {
+      // accessing the newly added property in the main function
+      res.send({time: req.time});
+    }
+  );
+/**********************************************/
+// GET route handler for ("/:word/echo") that uses Route Parameter Input from the User/Client and returns json
+// http://localhost:3000/HelloWorld/echo
+app.get("/:word/echo", (req, res) => {
+    res.json({echo: req.params.word}) ;
+  });
+/**********************************************/
+// GET route handler for ("/name") that uses Query Parameter Input from the User/Client and returns json
+// http://localhost:3000/name?first=luis&last=fonseca
+app.get("/name", (req, res) => {
+    res.json( {name: req.query.first + " " + req.query.last} ) ;
+  });
+/**********************************************/
  module.exports = app;
+/**********************************************/
